470b753cf5d96fc91de2b665d3215cf99c72db76
Shiva Manjunath <theblogdoctor@gmail.com>
2017-11-06 23:19:23 -0800

    Added PhoneForCreationDto.

---
+++ BondsBuddy.Api/Models/Dtos/PhoneForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BondsBuddy.Api.Models.Dtos
+{
+    public class PhoneForCreationDto
+    {
+        [MaxLength(80)]
+        public string PhoneName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string PhoneNumber { get; set; }
+    }
+}
