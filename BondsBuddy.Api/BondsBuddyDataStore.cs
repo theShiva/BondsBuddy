e28bb154dcb39c115eeb2af93987b615ff3b3e95
Shiva Manjunath <theblogdoctor@gmail.com>
2017-11-06 22:45:38 -0800

    Created in-memory data store for storing Phones and other entities.

---
+++ BondsBuddy.Api/BondsBuddyDataStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BondsBuddy.Api.Models;
+
+namespace BondsBuddy.Api
+{
+    public class BondsBuddyDataStore
+    {
+        public static BondsBuddyDataStore Current { get; } = new BondsBuddyDataStore();
+
+        public List<Phone> Phones { get; set; }
+
+        public BondsBuddyDataStore()
+        {
+            Phones = new List<Phone>()
+            {
+                new Phone()
+                {
+                    Id = 1,
+                    PhoneName = "Bill & Melinda Gates Foundation",
+                    PhoneNumber = "2067093140"
+                }
+            };
+        }
+    }
+}
