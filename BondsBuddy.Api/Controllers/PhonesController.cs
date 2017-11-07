f1c6e139ff6979b59497c6f2e01b8b64f561eff4
Shiva Manjunath <theblogdoctor@gmail.com>
2017-11-06 22:47:19 -0800

    Created Phones Controller.

---
+++ BondsBuddy.Api/Controllers/PhonesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Http;
+using BondsBuddy.Api.Models.Responses;
+
+namespace BondsBuddy.Api.Controllers
+{
+    [RoutePrefix("api/v1/phones")]
+    public class PhonesController : ApiController
+    {
+        [HttpGet, Route("", Name = "GetAllPhones")]
+        public IHttpActionResult Get()
+        {
+            var response = new PhonesResponse();
+
+            var phonesToReturn = BondsBuddyDataStore.Current.Phones.ToList();
+
+            response.Meta.HttpStatusCode = (int) HttpStatusCode.OK;
+            response.Phones = phonesToReturn;
+            return Ok(response);
+        }
+    }
+}
