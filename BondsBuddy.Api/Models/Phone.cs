3d986641cad2c356b4e6398e11a68e424a71f3df
Shiva Manjunath <theblogdoctor@gmail.com>
2017-11-07 21:53:43 -0800

    Normalized and refactored Phone model in (in-memory) database to include various properties extracted from parsing into a CanonicalPhoneNumber model.

--- BondsBuddy.Api/Models/Phone.cs
+++ BondsBuddy.Api/Models/Phone.cs
@@ -1,11 +1,29 @@
-﻿namespace BondsBuddy.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BondsBuddy.Api.Models
 {
     public class Phone
     {
+        [Required]
         public int Id { get; set; }
 
         public string PhoneName { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public int? CountryCode { get; set; }
+
+        public string Iso2CountryCode { get; set; }
+
+        public string E164PhoneNumber { get; set; }
+
+        public string NationalPhoneNumber { get; set; }
+
+        public string InternationalPhoneNumber { get; set; }
+
+        [Required]
+        public string RawPhoneNumber { get; set; }
+
+        public string NationalFormattedPhoneNumber { get; set; }
+       
     }
 }
