0b238d1b37c8f2baecb83abd31905dc7053824a6
Shiva Manjunath <theblogdoctor@gmail.com>
2017-11-07 22:08:46 -0800

    Updated PhoneDto to include normalized fields from the updated Phone model.

--- BondsBuddy.Api/Models/Dtos/PhoneDto.cs
+++ BondsBuddy.Api/Models/Dtos/PhoneDto.cs
@@ -7,5 +7,17 @@
         public string PhoneName { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public int? CountryCode { get; set; }
+
+        public string Iso2CountryCode { get; set; }
+
+        public string E164PhoneNumber { get; set; }
+
+        public string NationalPhoneNumber { get; set; }
+
+        public string InternationalPhoneNumber { get; set; }
+
+        public string RawPhoneNumber { get; set; }
     }
 }
